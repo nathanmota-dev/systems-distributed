f799c59c9abe5446972122a81504344a628d3d1e
Nathan Mota <nathansmota@gmail.com>
2025-04-18 17:55:38 -0300

    chore: config prisma for database

---
+++ backend/prisma/migrations/20250418203930_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('TEACHER', 'STUDENT');
+
+-- CreateEnum
+CREATE TYPE "VideoStatus" AS ENUM ('PENDING', 'PROCESSING', 'READY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "teacherId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "s3Url" TEXT NOT NULL,
+    "status" "VideoStatus" NOT NULL DEFAULT 'PENDING',
+    "courseId" TEXT NOT NULL,
+    "uploaderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_uploaderId_fkey" FOREIGN KEY ("uploaderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import helloRoutes from './routes/hello-routes';
+import prisma from './database/db';
 
 const app = Fastify();
 
@@ -7,10 +8,13 @@ app.register(helloRoutes, { prefix: "/" });
 
 const start = async () => {
     try {
+        await prisma.$connect();
+        console.log('Database connected!');
+
         await app.listen({ port: 8080 });
         console.log('Server is running at http://localhost:8080');
     } catch (error) {
-        app.log.error(error);
+        console.error(error);
         process.exit(1);
     }
 };
