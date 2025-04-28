00ed7c44a19cc96a2f0e89c67acc8a112f236dd3
Nathan Mota <nathansmota@gmail.com>
2025-04-28 04:44:01 -0300

    feat: creating new route for list videos

--- backend/src/routes/video-routes.ts
+++ backend/src/routes/video-routes.ts
@@ -1,7 +1,32 @@
 import { FastifyInstance } from 'fastify';
 import { confirmUploadController, presignVideoController } from '../controller/video-controller';
+import prisma from '../database/db';
 
 export default async function videoRoutes(app: FastifyInstance) {
     app.post('/videos/presign', presignVideoController);
     app.post('/videos/confirm-upload', confirmUploadController);
+
+    app.get('/videos/by-course/:courseId', async (request, reply) => {
+        const { courseId } = request.params as { courseId: string };
+
+        try {
+            const videos = await prisma.video.findMany({
+                where: { courseId },
+                orderBy: { createdAt: 'asc' },
+                select: {
+                    id: true,
+                    title: true,
+                    description: true,
+                    s3Url: true,
+                    status: true,
+                    createdAt: true,
+                }
+            });
+
+            return reply.send(videos);
+        } catch (error) {
+            console.error("Erro ao buscar vídeos:", error);
+            return reply.status(500).send({ error: "Erro interno ao buscar vídeos." });
+        }
+    });
 }
